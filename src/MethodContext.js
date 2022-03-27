823e4a81ac370e43681c73dff85b3dad526ae037
Mefrit <mihail_fokin_1999@mail.ru>
2022-03-27 16:51:48 +0300

    завершение выполнения создания  с объектами

--- src/MethodContext.js
+++ src/MethodContext.js
@@ -6,10 +6,19 @@ import { EventEmitter } from './EventEmitter';
 2)В методе unsubscribe отпишитесь от события click с помощью EventEmitter.off(eventName, callback).
 В качестве callback нужно передавать тот же самый обработчик, который был передан при подписке.
  */
+// increasecount.call({ count: this.count })
 export const obj = {
     count: 0,
-    subscribe() {},
-    unsubscribe() {},
+    callback: () => {},
+    subscribe() {
+        this.callback = () => {
+            this.count++;
+        };
+        EventEmitter.on('click', this.callback);
+    },
+    unsubscribe() {
+        EventEmitter.off('click', this.callback);
+    },
 };
 
 /*
@@ -18,8 +27,11 @@ export const obj = {
 obj1.first(1, 2, 3);
 // Внутренний вызов должен быть равносилен obj1.second(3, 2, 1)
  */
+
 export const obj1 = {
-    first(...args) {},
+    first(...args) {
+        this.second.apply(this, Object.values({ ...args }).reverse());
+    },
     second() {
         // здесь ничего писать не нужно
     },
